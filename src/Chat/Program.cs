c102259a47fa680f478cc71ac9574d6b37ca3fca
Sefer Mirza <sefermirza@gmail.com>
2024-11-29 15:48:39 +0300

    change port settings

--- src/Chat/Program.cs
+++ src/Chat/Program.cs
@@ -4,7 +4,7 @@ using Chat.Exceptions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.WebHost.UseKestrel();
 builder.Services.AddSignalR();
 builder.Services.AddOurServices();
 
